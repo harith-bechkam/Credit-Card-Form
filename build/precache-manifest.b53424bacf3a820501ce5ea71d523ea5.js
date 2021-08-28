fec76ec3414cad55e1a28a96f908aee03278ba09
harith-beckham <harith.kv47@gmail.com>
2021-08-28 21:03:58 +0530

    Added build folder to deploy it in netlify

---
+++ build/precache-manifest.b53424bacf3a820501ce5ea71d523ea5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d720ac059dab7f1c8308a3fad10962f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "046334de0092f095b06d",
+    "url": "/static/css/main.55be9441.chunk.css"
+  },
+  {
+    "revision": "58eaca39a9a85e9dcfa5",
+    "url": "/static/js/2.8515a15b.chunk.js"
+  },
+  {
+    "revision": "046334de0092f095b06d",
+    "url": "/static/js/main.ac7e43d4.chunk.js"
+  },
+  {
+    "revision": "d7953c62bc5ae7eeb673",
+    "url": "/static/js/runtime-main.a30b5898.js"
+  }
+]);
