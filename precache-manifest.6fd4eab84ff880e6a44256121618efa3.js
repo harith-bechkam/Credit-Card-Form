7f9849ee226950d2603c5f839d078d84caef921f
harith-beckham <harith.kv47@gmail.com>
2021-08-28 21:22:58 +0530

    Updates

---
+++ precache-manifest.6fd4eab84ff880e6a44256121618efa3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c1ddc49dd91acdaf22586288083f144",
+    "url": "/Credit-Card-Form/index.html"
+  },
+  {
+    "revision": "046334de0092f095b06d",
+    "url": "/Credit-Card-Form/static/css/main.55be9441.chunk.css"
+  },
+  {
+    "revision": "58eaca39a9a85e9dcfa5",
+    "url": "/Credit-Card-Form/static/js/2.8515a15b.chunk.js"
+  },
+  {
+    "revision": "046334de0092f095b06d",
+    "url": "/Credit-Card-Form/static/js/main.ac7e43d4.chunk.js"
+  },
+  {
+    "revision": "abe6b5d6b6b1b459212b",
+    "url": "/Credit-Card-Form/static/js/runtime-main.d6f7e790.js"
+  }
+]);
